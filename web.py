889f1a40b7379ec27c79a50da25a90dc74a8794a
shankarnarayanb07 <shankarnarayanab07@gmail.com>
2025-10-04 21:08:41 +0530

    Success

--- web.py
+++ web.py
@@ -1,60 +1,6 @@
-# EX01 Developing a Simple Webserver
-
-## Date: 04.10.2025
-
-## AIM:
-
-To develop a simple webserver to serve html pages and display the Device Specifications of your Laptop.
-
-## DESIGN STEPS:
-
-### Step 1:
-
-HTML content creation.
-
-### Step 2:
-
-Design of webserver workflow.
-
-### Step 3:
-
-Implementation using Python code.
-
-### Step 4:
-
-Import the necessary modules.
-
-### Step 5:
-
-Define a custom request handler.
-
-### Step 6:
-
-Start an HTTP server on a specific port.
-
-### Step 7:
-
-Run the Python script to serve web pages.
-
-### Step 8:
-
-Serve the HTML pages.
-
-### Step 9:
-
-Start the server script and check for errors.
-
-### Step 10:
-
-Open a browser and navigate to http://127.0.0.1:8000 (or the assigned port).
-
-## PROGRAM:
-
-....
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 content = """
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -99,25 +45,14 @@ content = """
 """
 
 class MyHandler(BaseHTTPRequestHandler):
-def do_GET(self):
-print("Request received")
-self.send_response(200)
-self.send_header('Content-type', 'text/html; charset=utf-8')
-self.end_headers()
-self.wfile.write(content.encode())
+    def do_GET(self):
+        print("Request received")
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(content.encode())
 
 server_address = ('', 8000)
 httpd = HTTPServer(server_address, MyHandler)
 print("My webserver is running...")
 httpd.serve_forever()
-
-....
-
-## OUTPUT:
-
-![alt text](<Screenshot (128).png>)
-![alt text](<Screenshot (129).png>)
-
-## RESULT:
-
-The program for implementing simple webserver is executed successfully.
